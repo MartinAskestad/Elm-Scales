20b3991fde0e8714d568c075705166bddfbff4cf
Martin Askestad <MartinAskestad@users.noreply.github.com>
2018-02-18 09:14:40 +0100

    Added gulp-task to deploy to gh-pages.

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const webpack = require('webpack');
+const webpackStream = require('webpack-stream');
+const ghPages = require('gulp-gh-pages');
+
+const webpackConfig = require('./webpack.config');
+
+gulp.task('webpack', () => {
+    return gulp.src('./src/bootstrap.js')
+        .pipe(webpackStream(webpackConfig, webpack))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('gh-pages', () => {
+    return gulp.src('./dist/**/*')
+        .pipe(ghPages());    
+});
+
+gulp.task('default', ['webpack', 'gh-pages']);
